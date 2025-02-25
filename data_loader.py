338badde1c297b07499d802aa947f08cb8442758
Sirotul Azhar <137380452+sirotulazhar@users.noreply.github.com>
2025-02-25 09:37:28 +0700

    Delete data_loader.py

--- data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file_path='cleaned_data.csv'):
-    df = pd.read_csv(file_path)
-    df['waktu'] = pd.to_datetime(df['waktu'], errors='coerce')
-    df['prov_sekolah'] = df['prov_sekolah'].str.strip().str.title()
-    df['kota_kab_sekolah'] = df['kota_kab_sekolah'].str.strip().str.title()
-    df['payment_method'] = df['payment_method'].str.strip().str.lower()
-    return df
