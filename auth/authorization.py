942c8bb306b610fadafef6877d1f3cad57f064b0
Sirotul Azhar <137380452+sirotulazhar@users.noreply.github.com>
2025-02-27 11:12:51 +0700

    Update authorization.py

--- auth/authorization.py
+++ auth/authorization.py
@@ -10,22 +10,22 @@ class RoleHandler:
 class SuperAdminHandler(RoleHandler):
     def handle(self, role, feature):
         if role == "superadmin":
-            return True  # Superadmin bisa akses semua fitur
+            restricted_features = ["Upload Data"]  # Gunakan nama menu yang benar
+            return feature not in restricted_features
         return super().handle(role, feature)
 
 class AdminHandler(RoleHandler):
     def handle(self, role, feature):
         if role == "admin":
-            restricted_features = ["Detail Harian Transaksi"]  # Gunakan nama menu yang benar
+            restricted_features = ["Detail Harian Transaksi", "Upload Data"]  # Gunakan nama menu yang benar
             return feature not in restricted_features  # Admin tidak bisa akses fitur ini
         return super().handle(role, feature)
 
-class UserHandler(RoleHandler):
+class UploaderHandler(RoleHandler):
     def handle(self, role, feature):
-        if role == "user":
-            allowed_features = ["Dashboard"]  # User hanya bisa akses Dashboard
+        if role == "uploader":
+            allowed_features = ["Upload Data"]  # Uploader hanya bisa akses fitur ini
             return feature in allowed_features
         return super().handle(role, feature)
 
-# Urutan pengecekan: SuperAdmin -> Admin -> User
-handler = SuperAdminHandler(AdminHandler(UserHandler()))
+handler = SuperAdminHandler(AdminHandler(UploaderHandler()))
