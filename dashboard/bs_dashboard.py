4f093ddcd486d8bbe5b8745067ed67a4866bc5dc
Sirotul Azhar <137380452+sirotulazhar@users.noreply.github.com>
2025-02-27 11:15:25 +0700

    Update bs_dashboard.py

--- dashboard/bs_dashboard.py
+++ dashboard/bs_dashboard.py
@@ -1,26 +1,14 @@
 import streamlit as st
 from abc import ABC, abstractmethod
-from dashboard.utils import format_rupiah
 
 class BaseDashboard(ABC):
     def __init__(self, df):
         self.df = df
 
     @abstractmethod
-    def filter_data(self):
-        pass
-
-    @abstractmethod
     def show_metrics(self):
         pass
-
-    @abstractmethod
-    def show_visualization(self):
-        pass
     
-
     def run(self):
-        self.filter_data()
         self.show_metrics()
-        self.show_visualization()
         
