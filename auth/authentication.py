0efad5c0f381e743c63a48c07af53223fef4c1c0
Sirotul Azhar <137380452+sirotulazhar@users.noreply.github.com>
2025-03-02 14:19:01 +0700

    Update authentication.py

--- auth/authentication.py
+++ auth/authentication.py
@@ -2,23 +2,8 @@ import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 
-# Mengambil variabel dari secrets Streamlit
-DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = st.secrets["DB_PORT"]
-DB_NAME = st.secrets["DB_NAME"]
-DB_USER = st.secrets["DB_USER"]
-DB_PASSWORD = st.secrets["DB_PASSWORD"]
-
-def get_db_connection():
-    """Membuat koneksi ke database PostgreSQL"""
-    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-    return engine
-
 def load_users():
-    """Mengambil data dari PostgreSQL"""
-    engine = get_db_connection()
-    query = "SELECT username, password, role FROM login"
-    return pd.read_sql(query, engine)
+    return pd.read_csv("data/login.csv")
 
 def authenticate(username, password):
     users = load_users()
