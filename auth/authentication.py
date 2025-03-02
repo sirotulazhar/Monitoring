8477dd6c507075df52b1886d7d62c30f99d0dacf
Sirotul Azhar <137380452+sirotulazhar@users.noreply.github.com>
2025-03-02 15:47:22 +0700

    Update authentication.py

--- auth/authentication.py
+++ auth/authentication.py
@@ -1,9 +1,31 @@
 import streamlit as st
-from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+import os
+
+# Mengambil konfigurasi dari secrets
+db_secrets = st.secrets.get("connections", {}).get("postgresql", {})
+
+DB_USER = db_secrets.get("DB_USER", "")
+DB_PASSWORD = db_secrets.get("DB_PASSWORD", "")
+DB_HOST = db_secrets.get("DB_HOST", "")
+DB_PORT = db_secrets.get("DB_PORT", "")
+DB_NAME = db_secrets.get("DB_NAME", "")
+
+def get_db_connection():
+        """Membuat koneksi ke database PostgreSQL"""
+        engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+        return engine
 
 def load_users():
-    return pd.read_csv("data/login.csv")
+    """Mengambil data dari PostgreSQL"""
+    engine = get_db_connection()
+    query = """
+    SELECT username, password, role
+    FROM login
+    """
+    return pd.read_sql(query, engine)
 
 def authenticate(username, password):
     users = load_users()
